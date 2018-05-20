ee0c802a136f78bce6bfe2e8eab3648adacac987
Drew Null <dnull@rightpoint.com>
2018-05-20 18:50:16 -0500

    Add dependency resolution.

---
+++ src/ClassicalMusicShop/ClassicalMusicShop.Website/Infrastructure/DependencyInjection/DependencyResolverInitialization.cs
@@ -0,0 +1,38 @@
+﻿namespace ClassicalMusicShop.Website.Infrastructure.DependencyInjection
+{
+    using System.Web.Mvc;
+    using EPiServer.Framework;
+    using EPiServer.Framework.Initialization;
+    using EPiServer.ServiceLocation;
+    using StructureMap;
+
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class DependencyResolverInitialization : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            var container = context.StructureMap();
+            container.Configure(ConfigureContainer);
+
+            DependencyResolver.SetResolver(new StructureMapDependencyResolver(container));
+        }
+
+        private static void ConfigureContainer(ConfigurationExpression container)
+        {
+            //Implementations for custom interfaces can be registered here.
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+
+        public void Preload(string[] parameters)
+        {
+        }
+    }
+}
