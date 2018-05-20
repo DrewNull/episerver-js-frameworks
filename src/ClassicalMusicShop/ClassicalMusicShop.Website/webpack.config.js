90f49ba8cad6330af5117abd62f80976ab79e496
Drew Null <dnull@rightpoint.com>
2018-05-20 14:23:06 -0500

    Stub compilation of client-side assets.

--- src/ClassicalMusicShop/ClassicalMusicShop.Website/webpack.config.js
+++ src/ClassicalMusicShop/ClassicalMusicShop.Website/webpack.config.js
@@ -1,1 +1,13 @@
-﻿
+﻿module.exports = {
+    devtool: 'source-map', 
+    entry: {
+        'main-angular': ['./static/js/main-angular.js'],
+        'main-react': ['./static/js/main-react.js'],
+        'main-vue': ['./static/js/main-vue.js']
+    }, 
+    mode: 'development',
+    output: {
+        filename: '[name].js', 
+        path: __dirname + '/static/dist'
+    }
+};
