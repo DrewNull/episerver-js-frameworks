55a35cc09bc97534ecef49bbd7c7bf8bd14b9a33
Drew Null <dnull@rightpoint.com>
2018-05-22 00:26:17 -0500

    Remove vueInitialization for readability.

--- src/ClassicalMusicShop/ClassicalMusicShop.Website/Static/js/main-vue.js
+++ src/ClassicalMusicShop/ClassicalMusicShop.Website/Static/js/main-vue.js
@@ -1,13 +1,31 @@
 ﻿console.log('Hello Vue!');
 
+import $ from 'jquery';
 import { CartService } from './cartService';
 import { CartPreview } from './vue/cartPreview';
 import { SheetMusicPage } from './vue/sheetMusicPage';
-import { VueInitialization } from './vue/vueInitialization';
 
 const cartService = new CartService();
 
-const cartPreviews    = VueInitialization.init('[cms-cart-preview]'    , (el) => new CartPreview(el, cartService)   );
-const sheetMusicPages = VueInitialization.init('[cms-sheet-music-page]', (el) => new SheetMusicPage(el, cartService));
+const cartPreviews =    initVue('[cms-cart-preview]'    , (el) => new CartPreview(el, cartService)   );
+const sheetMusicPages = initVue('[cms-sheet-music-page]', (el) => new SheetMusicPage(el, cartService));
 
 cartService.gettingPreview();
+
+function initVue(selector, createVue) {
+
+    let elements = $(selector);
+
+    let result = [];
+
+    for (let index = 0; index < elements.length; index++) {
+
+        let element = elements[index];
+
+        var vue = createVue(element);
+
+        result.push(vue);
+    }
+
+    return result;
+}
